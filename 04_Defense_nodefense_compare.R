ef7a30edce97916f246a7a9ca178b060336f8eaa
Charlotte Malmborg <malmborg@scc-tb1.scc.bu.edu>
2026-03-28 12:58:17 -0400

    first commit - getting comparison script ready

---
+++ 04_Defense_nodefense_compare.R
@@ -0,0 +1,9 @@
+### Script for running multiple runs and comparing against no-defense case ###
+
+## Load libraries
+library(dplyr)
+
+## Load functions
+source("/projectnb/dietzelab/malmborg/PestDefense/00_PestED_Defoliation.R")
+
+## Run a default with no defense added case:
