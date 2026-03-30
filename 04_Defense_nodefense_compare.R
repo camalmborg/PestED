e62777e95dac2ada444b5ab5c9598b9e88d17e50
Charlotte Malmborg <malmborg@scc-cc6.scc.bu.edu>
2026-03-30 12:40:41 -0400

    playing with defense efficiency and defoliation intensity

--- 04_Defense_nodefense_compare.R
+++ 04_Defense_nodefense_compare.R
@@ -13,8 +13,11 @@ source("/projectnb/dietzelab/malmborg/PestDefense/00_PestED_Defoliation.R")
 params$defenseAlloc = 0
 params$defenseBreakdown = 0
 params$defenseEfficiency = 0
+# set defense state variable to 0:
+X[8] = 0
 # run SEM no defense allocation:
-default_no_defense = iterate.SEM(c(0,0,0,1,0), years = 3)
+default_no_defense = iterate.SEM(c(0,0,0,1,0), years = 5)
+defol_no_def = iterate.SEM(c(0,0,1,1,0), years = 5)
 
 ## Run default with defense allocation but no defense efficiency
 # set param values:
@@ -23,11 +26,15 @@ defense_turnover_percent = 0.75  # turnover percent per day
 params$defenseAlloc = (defense_alloc_percent/100)/365/86400*timestep
 params$defenseBreakdown = (defense_turnover_percent/100)/86400*timestep
 params$defenseEfficiency = 0
+# set defense state variable to 0:
+X[8] = 0
 # run SEM with no defense efficiency:
-default_with_defense = iterate.SEM(c(0,0,0,1,0), years = 3)
+default_with_defense = iterate.SEM(c(0,0,0,1,0), years = 5)
 
 
-## Runs with defense allocation and varying efficiency values:
+## Runs with defense allocation and varying efficiency values
+# reset values:
+source("/projectnb/dietzelab/malmborg/PestDefense/00_PestED_Defoliation.R")
 # set param values:
 defense_alloc_percent = 70   # allocation percent for whole year
 defense_turnover_percent = 0.75  # turnover percent per day
@@ -35,4 +42,14 @@ params$defenseAlloc = (defense_alloc_percent/100)/365/86400*timestep
 params$defenseBreakdown = (defense_turnover_percent/100)/86400*timestep
 params$defenseEfficiency = 1
 # run SEM with defense and defoliation:
-defol <- iterate.SEM(c(0,0,1,1,0), years = 3)
+defol <- iterate.SEM(c(0,0,1,1,0), years = 5)
+defol2 <- iterate.SEM(c(0,0,1,1,0), years = 5)
+defol3 <- iterate.SEM(c(0,0,1,1,0), years = 5)
+
+# 100% defol over 4 weeks = 100/length(7000:11032) = 0.0248
+
+# 100% over 4 weeks = 0.0002479544
+# 75% over 4 weeks = 0.0001859658
+# 50% over 4 weeks = 0.0001239772
+# 25% over 4 weeks = 6.198859*10^5
+
--- 05_Defense_plots_figures_script.R
+++ 05_Defense_plots_figures_script.R
@@ -25,10 +25,14 @@ SEM_output_fx <- function(output, cols, model_run, years){
     mutate(year = rep(1:years, each = nrow(output)/years), .before = 3)
 }
 
-dnd <- SEM_output_fx(default_no_defense, cols = cols, model_run = 1, years = 3)
-dwd <- SEM_output_fx(default_with_defense, cols = cols, model_run = 2, years = 3)
-def <- SEM_output_fx(defol, cols = cols, model_run = 3, years = 3)
-all <- rbind(dnd, dwd, def)
+dnd <- SEM_output_fx(default_no_defense, cols = cols, model_run = 1, years = 5)
+dwd <- SEM_output_fx(default_with_defense, cols = cols, model_run = 2, years = 5)
+def_nd <- SEM_output_fx(defol_no_def, cols = cols, model_run = 3, years = 5)
+def <- SEM_output_fx(defol, cols = cols, model_run = 4, years = 5)
+def2 <- SEM_output_fx(defol2, cols = cols, model_run = 5, years = 5)
+def3 <- SEM_output_fx(defol3, cols = cols, model_run = 6, years = 5)
+#all <- rbind(dnd, dwd, def_nd, def, def2, def3)
+all <- rbind(dwd, def_nd, def, def2, def3)
 
 # selecting and processing for plots:
 var <- grep("Bleaf", cols)
@@ -37,12 +41,14 @@ bleaf <- all |>
   # select desired variable:
   select(-c(cols)[-c(var)]) |>
   # rename column for making plot:
-  rename(value = cols[var])
+  rename(value = cols[var]) #|>
+  #drop_na(value)
 
 ## Making plots
 test_plot <- ggplot(data = bleaf, aes(x = timestep, y = value, group = model_run, color = model_run)) +
-  geom_line() +
+  geom_line(linewidth = 1) +
   scale_color_gradient(low = "blue", high = "red") +
   theme_bw() +
-  theme(legend.position = "none")
+  theme(legend.position = "right",
+        panel.grid = element_blank())
 test_plot
