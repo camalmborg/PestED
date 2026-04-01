71ff757b89d4cb9e517a867c36568b471907f276
Charlotte Malmborg <malmborg@scc-yr1.scc.bu.edu>
2026-03-31 22:00:57 -0400

    changing file names and starting to make figure code repeatable

--- 01_Defense_alloc_turn_time_series_runs.R
+++ 01_Defense_alloc_turn_time_series_runs.R
@@ -13,22 +13,39 @@ alloc_turn_runs <- data.frame(model_run = 1:9,
                               turn = turn_runs,
                               ax_or = c("ax", "ax", "ax", "or", "or", "ctr", "or", "ax", "ax"))
 
+
+## Running the model and collecting results
+# make a list for storing time series:
+alloc_turn_results <- list()
 # years for time series:
 years = 5
 
+# loop:
+for (i in 1:nrow(alloc_turn_runs)){
+  # set param values:
+  defense_alloc_percent = alloc_turn_runs$alloc[i]   # allocation percent for whole year
+  defense_turnover_percent = alloc_turn_runs$turn[i]  # turnover percent per day
+  params$defenseAlloc = (defense_alloc_percent/100)/365/86400*timestep
+  params$defenseBreakdown = (defense_turnover_percent/100)/86400*timestep
+  params$defenseEfficiency = 1
+  
+  # run the model:
+  defol_model_run <- iterate.SEM(c(0,0,1,1,0), years = years)
+  
+  # save it:
+  name <- paste0(i, "_alloc_", defense_alloc_percent, 
+                 "_turnover_", defense_turnover_percent,
+                 "_postition_", alloc_turn_runs$ax_or[i])
+  alloc_turn_results[[name]] <- defol_model_run
+  rm(defol_model_run)
+}
 
-## Setting up model runs
+
+
+
+
+### Archive ###
+## Setting up model runs for parallel jobs
 # call the run:
-task_id <- as.numeric(Sys.getenv("SGE_TASK_ID"))
+#task_id <- as.numeric(Sys.getenv("SGE_TASK_ID"))
 
-# set param values:
-defense_alloc_percent = alloc_turn_runs$alloc[task_id]   # allocation percent for whole year
-defense_turnover_percent = alloc_turn_runs$turn[task_id]  # turnover percent per day
-params$defenseAlloc = (defense_alloc_percent/100)/365/86400*timestep
-params$defenseBreakdown = (defense_turnover_percent/100)/86400*timestep
-params$defenseEfficiency = 1
-
-
-## Running the model
-# run SEM with no defense efficiency:
-defol_model_run = iterate.SEM(c(0,0,1,1,0), years = years)
