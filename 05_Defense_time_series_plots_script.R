b66efab9271ae45734f8e61c1c8f626bbfdaa6da
Charlotte Malmborg <malmborg@scc-yr1.scc.bu.edu>
2026-03-31 22:01:09 -0400

    making plots repeatable

---
+++ 05_Defense_time_series_plots_script.R
@@ -0,0 +1,58 @@
+### Making results plots for comparing outputs ###
+
+## Load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(patchwork)
+
+## Processing data for making plots
+# selecting desired columns for figures:
+cols <- c("Bwood", "Bleaf", "Bstore", "Bdefense", "density")
+
+# processing data function:
+SEM_output_fx <- function(output, cols, model_run, years){
+  model_outputs <- as.data.frame(output) |>
+    # select columns for plots:
+    select(all_of(cols)) |>
+    # add a column for analysis group:
+    mutate(model_run = model_run, .before = 1) |>
+    # adding column for timestep:
+    mutate(timestep = 1:nrow(output), .before = 2) |>
+    # add column for years:
+    mutate(year = rep(1:years, each = nrow(output)/years), .before = 3)
+}
+
+## Making figures:
+# 
+
+
+# selecting and processing for plots:
+var <- grep("density", cols)
+
+bleaf <- all |>
+  # select desired variable:
+  select(-c(cols)[-c(var)]) |>
+  # rename column for making plot:
+  rename(value = cols[var]) #|>
+  #drop_na(value)
+
+## Making plots
+test_plot <- ggplot(data = bleaf, aes(x = timestep, y = value, group = model_run, color = model_run)) +
+  geom_line(linewidth = 1) +
+  scale_color_gradient(low = "blue", high = "red") +
+  theme_bw() +
+  theme(legend.position = "right",
+        panel.grid = element_blank())
+test_plot
+
+
+### Archive ###
+# dnd <- SEM_output_fx(default_no_defense, cols = cols, model_run = 1, years = 5)
+# dwd <- SEM_output_fx(default_with_defense, cols = cols, model_run = 2, years = 5)
+# def_nd <- SEM_output_fx(defol_no_def, cols = cols, model_run = 3, years = 5)
+# def <- SEM_output_fx(defol, cols = cols, model_run = 4, years = 5)
+# def2 <- SEM_output_fx(defol2, cols = cols, model_run = 5, years = 5)
+# def3 <- SEM_output_fx(defol3, cols = cols, model_run = 6, years = 5)
+# #all <- rbind(dnd, dwd, def_nd, def, def2, def3)
+# all <- rbind(def_nd, def, def2, def3)
