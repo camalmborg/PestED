6978a1ee89f764b84689783c8b787d10e37f5de1
Charlotte Malmborg <malmborg@scc-cc7.scc.bu.edu>
2026-04-07 05:00:05 -0400

    renaming this for now

---
+++ 02_Defense_efficiency_and_defoliation_analyses.R
@@ -0,0 +1,85 @@
+### Defense Efficiency and Defoliation time series ###
+
+# (1) two low-level defoliation cases (5%, 10% defoliation annually)
+# (2) one-time 100% defoliation case 
+
+## Load libraries
+library(dplyr)
+
+## Load functions
+source("/projectnb/dietzelab/malmborg/PestDefense/00_PestED_Defoliation.R")
+
+# ## Making a ranges of allocation and turnover values (based on literature):
+# # quantiles:
+# quants <- seq(0, 1, by = 0.05)
+# # testing values based on quantiles
+# defense_efficiencies <- quants    # defense efficiency
+# defoliation <- quants           # defoliation
+# 
+# ## Outputs:
+# default_result <- list()
+# defol_result <- list()
+# 
+# ## Looping over all values:
+# # defense efficiency loop:
+# for (i in 1:length(defense_efficiencies)){
+#   # set parameter value:
+#   params$defenseEfficiency = defense_efficiencies[i]
+#   
+#   # matrices of results:
+#   output_default <- matrix(NA, nrow = length(quants), ncol = 13)
+#   output_defol <- matrix(NA, nrow = length(quants), ncol = 13)
+#   colnames(output_default) <- varnames
+#   colnames(output_defol) <- varnames
+#   
+#   # defoliation magnitude loop:
+#   for (j in 1:length(defoliation)){
+#     # run default (no defoliation case):
+#     default <- iterate.SEM(c(0,0,0,1,0), years = 4)
+#     defol <- iterate.SEM(c(0,0,defoliation[j],1,0), years = 4)
+#     
+#     # get the output summaries:
+#     output_default[j,] <- 1-apply(default,2,min)/apply(default,2,max)
+#     output_defol[j,] <- 1-apply(defol,2,min)/apply(defol,2,max)
+#   }
+#   # all result:
+#   default_result[[i]] <- output_default
+#   defol_result[[i]] <- output_defol
+# }
+# 
+# ## save lists:
+# saveRDS(default_result, file = "/projectnb/dietzelab/malmborg/Ch3_PestDefense/DED_default_result.rds")
+# saveRDS(defol_result, file = "/projectnb/dietzelab/malmborg/Ch3_PestDefense/DED_defol_result.rds")
+# 
+# 
+# ## Checking out results ###
+# # load:
+# default_list <- readRDS("/projectnb/dietzelab/malmborg/Ch3_PestDefense/DED_default_result.rds")
+# default_results <- default_list[[1]][1,]
+# # defolation results:
+# defol_list <- readRDS("/projectnb/dietzelab/malmborg/Ch3_PestDefense/DED_defol_result.rds")
+# 
+# ## Convert the defol_list to a dataframe
+# # extract first member of list:
+# all_defol <- as.data.frame(defol_list[[1]]) |>
+#   # add column for list/analysis number:
+#   mutate(run = 1, .before = 1) |>
+#   # add columns for defense efficiency and defoliation intensity:
+#   mutate(defense_efficiency = defense_efficiencies[1], .after = 1) |>
+#   mutate(defoliation = defoliation, .after = 2)
+# # run loop cbinding the rest of the list:
+# for (i in 2:length(defol_list)){
+#   list_member <- as.data.frame(defol_list[[i]]) |>
+#     # add column for list/analysis number:
+#     mutate(run = i, .before = 1) |>
+#     # add columns for defense efficiency and defoliation intensity:
+#     mutate(defense_efficiency = defense_efficiencies[i], .after = 1) |>
+#     mutate(defoliation = defoliation, .after = 2)
+#   # bind to dataframe:
+#   all_defol <- rbind(all_defol, list_member)
+#   rm(list_member)
+# }
+# 
+# 
+
+
