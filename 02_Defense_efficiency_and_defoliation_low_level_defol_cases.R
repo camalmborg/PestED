0f6b89d4a88d373ed71e1a5ceaf680ed64d8b35e
Charlotte Malmborg <malmborg@scc-cc7.scc.bu.edu>
2026-04-07 06:34:22 -0400

    adding code for time series plots and combining plots

--- 02_Defense_efficiency_and_defoliation_low_level_defol_cases.R
+++ 02_Defense_efficiency_and_defoliation_low_level_defol_cases.R
@@ -1,8 +1,9 @@
 ### Script for testing defense efficiency and defoliation balance parameters ###
 
 # (1) Roll of defense allocation for low-lying annual defoliation 
-#  a) 5% defoliation with 0.25-1 defense efficiency
-#  b) 10% defoliation with 0.25-1 defense efficiency
+#  a) 5% defoliation with 0-1 defense efficiency
+#  b) 10% defoliation with 0-1 defense efficiency
+#  c) 15% defoliation with 0-1 defense efficiency
 
 ## libraries:
 library(dplyr)
@@ -22,6 +23,7 @@ def_eff <- c(0, 0.1, 0.25, 0.5, 1)
 # make lists to collect results:
 def_eff_defol_5pc_result <- list()
 def_eff_defol_10pc_result <- list()
+def_eff_defol_15pc_result <- list()
 
 # loops for running models:
 for (i in 1:length(def_eff)){
@@ -39,6 +41,8 @@ for (i in 1:length(def_eff)){
   defol_5pc <- iterate.SEM(c(0,0,0.05,1,0), t.start = defol_days, years = years)
   # run the model for 10% defoliation:
   defol_10pc <- iterate.SEM(c(0,0,0.1,1,0), t.start = defol_days, years = years)
+  # run the model for 15% defoliation:
+  defol_15pc <- iterate.SEM(c(0,0,0.15,1,0), t.start = defol_days, years = years)
     
   # set list member name:
   name <- paste0(i, "_def_eff_", def_eff[i])
@@ -46,6 +50,10 @@ for (i in 1:length(def_eff)){
   # add results to lists:
   def_eff_defol_5pc_result[[name]] <- defol_5pc
   def_eff_defol_10pc_result[[name]] <- defol_10pc
+  def_eff_defol_15pc_result[[name]] <- defol_15pc
+  
+  # remove loop results:
+  rm(defol_5pc, defol_10pc, defol_15pc)
 }
 
 
@@ -60,6 +68,7 @@ cols <- varnames[variables]
 names <- c("Wood", "Leaf", "Storage", "Defense", "Density")
 plot_units <- units[variables]
 
+# (1a: 5% defoliation case)
 # making plot data from alloc_turnover results list:
 time_series_data <- SEM_plot_data_fx(def_eff_defol_5pc_result) |>
   # add defense efficiency values for labeling:
@@ -81,5 +90,135 @@ density <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 5, runs
 combined <- (wood + leaf + store + density) + 
   plot_layout(ncol = 2, guides = "collect") & 
   theme(legend.position = "right")
-combined
+#combined
 
+# save plots:
+save_dir <- "/projectnb/dietzelab/malmborg/Ch3_PestDefense/defense_efficiency_defoliation/Figures/"
+png(filename = paste0(save_dir, Sys.Date(), "_5pc_annual_defol_time_series_plots.png"),
+    height = 10, width = 15, units = "in", res = 600)
+combined
+dev.off()
+
+# save wood, leaf, density in separate lists:
+wood_plots <- list()
+leaf_plots <- list()
+density_plots <- list()
+# save 5pc group:
+wood_plots[[1]] <- wood + guides(color = guide_legend(title="Defense Efficiency"))
+leaf_plots[[1]] <- leaf + guides(color = "none")
+density_plots[[1]] <- density + guides(color = "none")
+
+
+# (1b: 10% defoliation case)
+# making plot data from alloc_turnover results list:
+time_series_data <- SEM_plot_data_fx(def_eff_defol_10pc_result) |>
+  # add defense efficiency values for labeling:
+  mutate(def_eff = def_eff[model_run])
+
+# model runs chosen for example plots:
+lines = c(1:5)
+# labels for lines:
+labels <- as.character(def_eff)
+
+# making plots:
+wood <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 1, runs = lines, labels = labels)
+leaf <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 2, runs = lines, labels = labels)
+store <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 3, runs = lines, labels = labels)
+defense <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 4, runs = lines, labels = labels)
+density <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 5, runs = lines, labels = labels)
+
+# combining plots:
+combined <- (wood + leaf + store + density) + 
+  plot_layout(ncol = 2, guides = "collect") & 
+  theme(legend.position = "right")
+#combined
+
+# save plots:
+png(filename = paste0(save_dir, Sys.Date(), "_10pc_annual_defol_time_series_plots.png"),
+    height = 10, width = 15, units = "in", res = 600)
+combined
+dev.off()
+
+# save 10pc group:
+wood_plots[[2]] <- wood + guides(color = "none") + theme(plot.title = element_blank())
+leaf_plots[[2]] <- leaf + guides(color = "none") + theme(plot.title = element_blank())
+density_plots[[2]] <- density + guides(color = "none") + theme(plot.title = element_blank())
+
+
+# (1c: 15% defoliation case)
+# making plot data from alloc_turnover results list:
+time_series_data <- SEM_plot_data_fx(def_eff_defol_15pc_result) |>
+  # add defense efficiency values for labeling:
+  mutate(def_eff = def_eff[model_run])
+
+# model runs chosen for example plots:
+lines = c(1:5)
+# labels for lines:
+labels <- as.character(def_eff)
+
+# making plots:
+wood <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 1, runs = lines, labels = labels)
+leaf <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 2, runs = lines, labels = labels)
+store <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 3, runs = lines, labels = labels)
+defense <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 4, runs = lines, labels = labels)
+density <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 5, runs = lines, labels = labels)
+
+# combining plots:
+combined <- (wood + leaf + store + density) + 
+  plot_layout(ncol = 2, guides = "collect") & 
+  theme(legend.position = "right")
+#combined
+
+# save plots:
+png(filename = paste0(save_dir, Sys.Date(), "_15pc_annual_defol_time_series_plots.png"),
+    height = 10, width = 15, units = "in", res = 600)
+combined
+dev.off()
+
+# save 15pc group:
+wood_plots[[3]] <- wood + guides(color = "none") + theme(plot.title = element_blank())
+leaf_plots[[3]] <- leaf + guides(color = "none") + theme(plot.title = element_blank())
+density_plots[[3]] <- density + guides(color = "none") + theme(plot.title = element_blank())
+
+
+## making plots combining wood, leaf, density:
+# row labels:
+row_label_1 <- wrap_elements(panel = textGrob('5% Defoliation', rot=90, gp = gpar(fontsize = 14)))
+row_label_2 <- wrap_elements(panel = textGrob('10% Defoliation', rot=90, gp = gpar(fontsize = 14)))
+row_label_3 <- wrap_elements(panel = textGrob('15% Defoliation', rot=90, gp = gpar(fontsize = 14)))
+# combining plots:
+combined <- (row_label_1 + wood_plots[[1]] + leaf_plots[[1]] + density_plots[[1]] +
+             row_label_2 + wood_plots[[2]] + leaf_plots[[2]] + density_plots[[2]] +
+             row_label_3 + wood_plots[[3]] + leaf_plots[[3]] + density_plots[[3]]) + 
+  plot_layout(ncol = 4, widths = c(0.5, 3, 3, 3), guides = "collect") & 
+  theme(legend.position = "right",
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 14)) 
+#combined
+
+# save plots:
+png(filename = paste0(save_dir, Sys.Date(), "_all_annual_defol_time_series_plots.png"),
+    height = 12, width = 16, units = "in", res = 600)
+combined
+dev.off()
+
+
+
+
+
+### ARCHIVE ###
+# ## combining 0 defense efficiency cases:
+# def_eff_0 <- list(d_5 <- as.data.frame(def_eff_defol_5pc_result$`1_def_eff_0`),
+#                   d_10 <- as.data.frame(def_eff_defol_10pc_result$`1_def_eff_0`),
+#                   d_15 <- as.data.frame(def_eff_defol_15pc_result$`1_def_eff_0`))
+# 
+# # model runs chosen for example plots:
+# lines = c(1:3)
+# # labels for lines:
+# labels <- as.character(c(5,10,15))
+# 
+# # making plot data from alloc_turnover results list:
+# time_series_data <- SEM_plot_data_fx(def_eff_0)
+# 
+# density <- time_series_plot_fx(ts = time_series_data, cols = cols, var = 5, runs = lines, labels = labels)
+# 
