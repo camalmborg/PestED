cbf4be6ebabbf557390440b195cf08429b012e66
Charlotte Malmborg <malmborg@scc-dd2.scc.bu.edu>
2026-04-08 10:32:30 -0400

    making a few bar plots with outbreak defoliation

--- 01_Defense_alloc_outbreak_defol_cases.R
+++ 01_Defense_alloc_outbreak_defol_cases.R
@@ -218,4 +218,25 @@ dev.off()
 
 
 ## Bar plots
+# process and combine outputs:
+bar_plot_data <- rbind(SEM_bar_data_fx(alloc_defol_50pc_result, group = "50%"),
+                       SEM_bar_data_fx(alloc_defol_75pc_result, group = "75%"),
+                       SEM_bar_data_fx(alloc_defol_100pc_result, group = "100%"))
+
+# labels specifying groups:
+labels <- as.character(c(0.03, 0.14, 0.25, 0.36, 0.47))
+# number of runs:
+lines = c(1:length(defense_alloc_percent))
+
+# make plots:
+wood <- SEM_bar_plot_fx(bar_plot_data, cols = cols, var = 1, runs = lines, labels = labels) +
+  labs(x = "Allocation")
+leaf <- SEM_bar_plot_fx(bar_plot_data, cols = cols, var = 2, runs = lines, labels = labels) +
+  labs(x = "Allocation")
+store <- SEM_bar_plot_fx(bar_plot_data, cols = cols, var = 3, runs = lines, labels = labels) +
+  labs(x = "Allocation")
+defense <- SEM_bar_plot_fx(bar_plot_data, cols = cols, var = 4, runs = lines, labels = labels) +
+  labs(x = "Allocation")
+density <- SEM_bar_plot_fx(bar_plot_data, cols = cols, var = 5, runs = lines, labels = labels) +
+  labs(x = "Allocation")
 
